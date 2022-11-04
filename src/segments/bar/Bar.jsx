ae566f215b00d4cdcaa8e2f58c20dc885803d220
Dan <pintea.dan988@gmail.com>
2022-11-04 19:51:53 +0100

    Implemented BarChart.jsx && Bar.jsx.

--- src/segments/bar/Bar.jsx
+++ src/segments/bar/Bar.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import {Box, useTheme} from "@mui/material";
 
-function Bar(props) {
+import Header from "../../components/shared/Header";
+import BarChart from "../../components/BarChart";
+import {tokens} from "../../themes/themes";
+
+
+function Bar() {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+
     return (
-        <div>Bar</div>
+        <Box m="30px">
+            <Header title="Bar Chart" subtitle="Simple Bar Chart with dummy data (your API calls could be here)" color={colors.greenAccent[500]}/>
+            <Box height="75vh">
+                <BarChart />
+            </Box>
+        </Box>
     );
 }
 
